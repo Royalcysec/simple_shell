ed46d5678ab88cb6a50dd96e1f16b45f30e8bc1c
liliyosa <liliyosambakureya@gmail.com>
2022-07-30 09:11:31 -0700

    err_msgs2.c

--- err_msgs2.c
+++ err_msgs2.c
@@ -4,10 +4,10 @@ char *error_126(char **args);
 char *error_127(char **args);
 
 /**
- * error_126 - Creates an error message for permission denied failures.
- * @args: An array of arguments passed to the command.
+ * error_126 - Creates an error message for permission denied failures
+ * @args: An array of arguments passed to the command
  *
- * Return: The error string.
+ * Return: The error string
  */
 char *error_126(char **args)
 {
@@ -41,7 +41,7 @@ char *error_126(char **args)
  * error_127 - Creates an error message for command not found failures.
  * @args: An array of arguments passed to the command.
  *
- * Return: The error string.
+ * Return: The error string
  */
 char *error_127(char **args)
 {
