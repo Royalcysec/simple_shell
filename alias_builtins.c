9e0373d964b10cbdafb601e2c0fec305c4ac4adf
liliyosa <liliyosambakureya@gmail.com>
2022-07-30 08:18:32 -0700

    alias_builtins.c

--- alias_builtins.c
+++ alias_builtins.c
@@ -8,7 +8,7 @@ void print_alias(alias_t *alias);
  * shellby_alias - Builtin command that either prints all aliases, specific
  * aliases, or sets an alias.
  * @args: An array of arguments.
- * @front: A double pointer to the beginning of args.
+ * @front: A double pointer to the beginning of args
  *
  * Return: If an error occurs - -1.
  *         Otherwise - 0.
@@ -55,7 +55,7 @@ int shellby_alias(char **args, char __attribute__((__unused__)) **front)
 /**
  * set_alias - Will either set an existing alias 'name' with a new value,
  * 'value' or creates a new alias with 'name' and 'value'.
- * @var_name: Name of the alias.
+ * @var_name: Name of the alias
  * @value: Value of the alias. First character is a '='.
  */
 void set_alias(char *var_name, char *value)
@@ -113,7 +113,7 @@ void print_alias(alias_t *alias)
 /**
  * replace_aliases - Goes through the arguments and replace any matching alias
  * with their value.
- * @args: 2D pointer to the arguments.
+ * @args: 2D pointer to the arguments
  *
  * Return: 2D pointer to the arguments.
  */
@@ -150,4 +150,3 @@ char **replace_aliases(char **args)
 
 	return (args);
 }
-
